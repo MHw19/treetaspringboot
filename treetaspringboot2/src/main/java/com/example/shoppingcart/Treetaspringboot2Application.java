55855c0276a49ca41c3afc5feb6593a9216a5bc6
Malinga Hashan Wiswajith Ekanayaka <40067206+MHw19@users.noreply.github.com>
2021-04-01 02:08:16 +0530

    initial commit
    adding with user controller

---
+++ treetaspringboot2/src/main/java/com/example/shoppingcart/Treetaspringboot2Application.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Treetaspringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Treetaspringboot2Application.class, args);
+	}
+
+}
---
+++ treetaspringboot2/src/test/java/com/example/shoppingcart/Treetaspringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Treetaspringboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
